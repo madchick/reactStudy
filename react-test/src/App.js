df5edd65888ea75c8de4f00501967f235506cca7
madchick <madchick@gmail.com>
2020-07-02 23:43:05 +0900

    Add TOC component event

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
     super(props);
     this.state = {
       mode:'read',
-      whelcome:{title:'Welcome', desc:'Hello, React!!!'},
+      selected_content_id:2,
       subject:{title:'WEB', sub:'world wide web'},
+      welcome:{title:'Welcome', desc:'Hello, React!!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -22,11 +23,18 @@ class App extends Component {
     console.log('App render');
     var _title, _desc = null;
     if(this.state.mode === 'welcome') {
-      _title = this.state.whelcome.title;
-      _desc = this.state.whelcome.desc; 
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc; 
     } else if(this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc; 
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
@@ -46,7 +54,15 @@ class App extends Component {
             this.setState({mode:'welcome'});
           }.bind(this)}
         ></Subject>        
-        <TOC data={this.state.contents}></TOC>
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- react-test/src/components/TOC.js
+++ react-test/src/components/TOC.js
@@ -6,7 +6,19 @@ function TOC(props) {
   var data = props.data;
   var i = 0;
   while(i<data.length) {
-    lists.push(<li key={data[i].id}><a href={"/contents/"+data[i].id}>{data[i].title}</a></li>);
+    lists.push(
+      <li key={data[i].id}>
+        <a 
+          href={"/contents/"+data[i].id}
+          data-id={data[i].id}
+          onClick={function(id, num, e){
+            e.preventDefault();
+            // props.onChangePage(e.target.dataset.id); - 이것도 동작함
+            props.onChangePage(id);
+          }.bind(this, data[i].id, 10)}
+        >{data[i].title}</a>
+      </li>
+    );
     i = i + 1;
   }
   return (
