25af9dc45dbf18b8e4da4ce6caeae523d1e4d704
madchick <madchick@gmail.com>
2020-10-04 13:59:02 +0900

    Updates

---
+++ precache-manifest.8a75cd377948078415943c47ce9b8405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e20bc8ee97173f704c5703acd41952",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c3528271d6d6a173fef9",
+    "url": "/movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "c3528271d6d6a173fef9",
+    "url": "/movie_app/static/js/main.099759a6.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
