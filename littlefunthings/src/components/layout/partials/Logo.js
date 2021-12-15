1508b0073aba44892715e9aa986eb800a1237123
madchick <madchick@Madchicks-MacBookPro-M1.local>
2021-12-15 15:41:55 +0900

    Fix miss typings

--- littlefunthings/src/components/layout/partials/Logo.js
+++ littlefunthings/src/components/layout/partials/Logo.js
@@ -14,20 +14,7 @@ const Logo = ({
   );
 
   return (
-    <div
-      {...props}
-      className={classes}
-    >
-      <h1 className="m-0">
-        <Link to="/">
-          <Image
-            src={require('./../../../assets/images/logo.svg')}
-            alt="Open"
-            width={32}
-            height={32} />
-        </Link>
-      </h1>
-    </div>
+    <div></div>
   );
 }
 
