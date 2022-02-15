d1da0670a18d237434a2a080b9655cec5071a85f
madchick <madchick@gmail.com>
2022-02-15 21:47:15 +0900

    Delete some sample projects

--- sm-test/hello-node/models/company.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var companySchema = new Schema({
-    companyName: String, service: String, maintenanceTeam: String, dutyed: String
-});
-
-module.exports = mongoose.model('company', companySchema, 'companys');
-//스키마명, 스키마 객체, 컬랙션명(db에 실제 있는)
--- sm-test/hello-node/models/session.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var sessionSchema = new Schema({
-    session:String
-});
-
-module.exports = mongoose.model('session', sessionSchema, 'sessions');
-//스키마명, 스키마 객체, 컬랙션명(db에 실제 있는)
