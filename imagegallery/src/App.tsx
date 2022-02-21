7f03ef7cb251035a0c623d1850e7055553ba391b
madchick <madchick@gmail.com>
2022-02-21 20:57:46 +0900

    Add imagegallery and delete movie_app

---
+++ imagegallery/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='container'>
+        <div className='initial-box'>
+          <div className='text-center'>
+            이미지가 없습니다.<br/>
+            이미지츨 추가해주세요.
+          </div>
+          <div className='plus-box'>
+            +
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
