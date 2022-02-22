7ceaeb3168d906e7816c36da056215416cc154c5
madchick <madchick@gmail.com>
2022-02-22 14:26:44 +0900

    Add drag and drop feature

--- imagegallery/src/App.tsx
+++ imagegallery/src/App.tsx
@@ -1,50 +1,55 @@
-import { render } from '@testing-library/react';
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './App.css';
 import ImageBox from './components/ImageBox';
 
+
 function App() {
-  const inpRef = useRef<HTMLInputElement>(null)
+	const [imageList, setImageList] = useState<string[]>([])
 
-  const [ImageList, setImageList] = useState<string[]>([])
-  console.log(ImageList)
+	const onDrop = useCallback(acceptedFiles => {
+		console.log(acceptedFiles)
+		if (acceptedFiles.length) {
 
-  return (
-    <div>
-      <div className='container'>
-        <div className={'gallery-box ' + (ImageList.length>0 && 'row')}>
-          {
-            ImageList.length === 0 &&
-            <div className='text-center'>
-              이미지가 없습니다.<br/>
-              이미지츨 추가해주세요.
-            </div>
-          }
-          <input type="file" ref={(inpRef)}
-            onChange={(event)=>{
-              if(event.currentTarget.files?.[0]) {
-                const file = event.currentTarget.files[0];
-                console.log(file.name);
 
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onloadend = (event) => {
-                  setImageList(prev => [...prev, event.target?.result as string]);
-                  console.log(ImageList);
-                }
-              }
-            }}/>
-          {
-            ImageList.map((el,idx) => <ImageBox key={el+idx} src={el} />)
-          }            
-          <div className='plus-box'
-            onClick={()=>{inpRef.current?.click()}}>
-            +
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+			for (const file of acceptedFiles) {
+				const reader = new FileReader();
+
+				reader.readAsDataURL(file)
+				reader.onloadend = (event) => {
+					setImageList(prev => [...prev, event.target?.result as string])
+				}
+			}
+		}
+	}, [])
+
+	const { getRootProps, getInputProps } = useDropzone({ onDrop })
+
+
+	return (
+		<div className='container'>
+			<div className={'gallery-box ' + (imageList.length > 0 && 'row')}>
+				{
+					imageList.length === 0 &&
+					<div className='text-center'>
+						이미지가 없습니다.<br />
+						이미지를 추가해주세요.
+					</div>
+				}
+				{
+					imageList.map((el, idx) => <ImageBox key={el + idx} src={el} />)
+				}
+				<div className='plus-box'
+					{...getRootProps()}
+				>
+					<input
+						{...getInputProps()}
+					/>
+					+
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- imagegallery/src/components/ImageBox.tsx
+++ imagegallery/src/components/ImageBox.tsx
@@ -1,9 +1,9 @@
-function ImageBox(props:{
-    src:string;
+function ImageBox(props: {
+	src: string;
 }) {
-    return <div className="image-box">
-        <img src={props.src}/>
-    </div>
+	return <div className="image-box">
+		<img src={props.src} />
+	</div>
 }
 
 export default ImageBox;
